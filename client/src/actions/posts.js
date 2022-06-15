26ee0c55f58a21b81195805aae75768b8aa5b430
Alperen Kilinc <kilincfurkanalperen@gmail.com>
2022-06-15 18:02:11 +0300

    Added form

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -2,16 +2,23 @@ import * as api from '../api';
 
 //Action Creators
 
-const getPosts = () => async (dispatch) => {
+export const getPosts = () => async (dispatch) => {
   try {
     const {data} = await api.fetchPosts();
-    dispatch(action);
-  } catch (error) {
+    dispatch( { type: 'FETCH ALL', payload: []});
+  } 
+  catch (error) {
+    console.log(error.message);
     
   }
-  
-  
-    const action = { type: 'FETCH ALL', payload: []}
+}
 
-    dispatch(action);
+export const createPost = (post) => async (dispatch) => {
+  try{
+      const {data} = await api.createPost(post);
+
+      dispatch({ type: 'CREATE', payload: data});
+  } catch (error) {
+   console.log(error);
+  }
 }
